28dcc7e8c7f807c48d287af73262c46218fac81f
xxqhh <774992883@qq.com>
2018-07-29 00:27:09 +0800

    fix #15 添加index.html页面

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*
 # 主页的路由
+from flask import Blueprint, render_template
+
+front = Blueprint('front', __name__)
+@front.route('/')
+def index():
+	return render_template('index.html')
